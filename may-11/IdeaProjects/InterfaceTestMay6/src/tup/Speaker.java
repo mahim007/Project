5dc48a00d7089e085dc50323affc9c4c8fefb331
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2018-05-25 15:24:45 +0600

    ok

--- may-11/IdeaProjects/InterfaceTestMay6/src/tup/Speaker.java
+++
@@ -1,9 +0,0 @@
-package tup;
-
-/**
- * Created by ICT LAB PC on 5/6/17.
- */
-public interface Speaker {
-
-    public void speak();
-}
--- may-11/IdeaProjects/Test1/src/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ICT LAB PC on 3/29/17.
- */
-public class Test {
-    public static void main(String[] args)
-    {
-        System.out.println("Assalmaulykum!");
-    }
-}
