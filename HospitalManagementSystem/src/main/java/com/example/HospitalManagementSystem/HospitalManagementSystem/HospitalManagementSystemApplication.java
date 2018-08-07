b075bab467cd6af569903bc0770960f5068567bf
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2018-08-08 01:40:29 +0600

    ok

---
+++ HospitalManagementSystem/src/main/java/com/example/HospitalManagementSystem/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.HospitalManagementSystem.HospitalManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+	}
+}
